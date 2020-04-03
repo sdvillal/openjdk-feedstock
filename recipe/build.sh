10aa0773c4182904307135e4371443191f766309
santi <sdvillal@gmail.com>
2020-04-03 07:37:50 +0200

    simplify build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,54 +1,25 @@
 #!/bin/bash -euo
 
-function jdk_install
-{
-  chmod +x bin/*
-  mkdir -p $INSTALL_DIR/bin
-  mv bin/* $INSTALL_DIR/bin/
-  ls -la $INSTALL_DIR/bin
+find . -maxdepth 1 \
+       -type d \
+       -not \( -name '.' -or -name 'fonts' -or -name 'demo' \) \
+       -exec sh -c "mkdir $PREFIX/$(basename {}); mv $(basename {})/* $PREFIX/$(basename {})/" \;
 
-  mkdir -p $INSTALL_DIR/include
-  mv include/* $INSTALL_DIR/include
-  if [ -e ./lib/jspawnhelper ]; then
-    chmod +x ./lib/jspawnhelper
-  fi
-
-  mkdir -p $INSTALL_DIR/lib
-  mv lib/* $INSTALL_DIR/lib
-
-  if [ -f "DISCLAIMER" ]; then
+if [ -f "DISCLAIMER" ]; then
     mv DISCLAIMER $INSTALL_DIR/DISCLAIMER
-  fi
-
-  mkdir -p $INSTALL_DIR/conf
-  mv conf/* $INSTALL_DIR/conf
-
-  mkdir -p $INSTALL_DIR/jmods
-  mv jmods/* $INSTALL_DIR/jmods
-
-  mkdir -p $INSTALL_DIR/legal
-  mv legal/* $INSTALL_DIR/legal
-
-  mkdir -p $INSTALL_DIR/man/man1
-  mv man/man1/* $INSTALL_DIR/man/man1
-  rm -rf man/man1
-  mv man/* $INSTALL_DIR/man
-}
-
-export INSTALL_DIR=$PREFIX
-jdk_install
+fi
 
 if [[ "$target_platform" == linux* ]]; then
-  mv $INSTALL_DIR/lib/jli/*.so $INSTALL_DIR/lib/
   # Include dejavu fonts to allow java to work even on minimal cloud
   # images where these fonts are missing (thanks to @chapmanb)
-  mkdir -p $INSTALL_DIR/lib/fonts
-  mv $SRC_DIR/fonts/ttf/* $INSTALL_DIR/lib/fonts/
-  # Have libjvm.so show up in the right place
-  ln -s $INSTALL_DIR/lib/server/libjvm.so $INSTALL_DIR/lib/libjvm.so
-fi
-find $PREFIX -name "*.debuginfo" -exec rm -rf {} \;
+  mkdir -p $PREFIX/lib/fonts
+  mv $SRC_DIR/fonts/ttf/* $PREFIX/lib/fonts/
 
+  # Have libjvm.so show up in the right place
+  ln -s $PREFIX/lib/server/libjvm.so $PREFIX/lib/libjvm.so
+fi
+
+find $PREFIX -name "*.debuginfo" -exec rm -rf {} \;
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
